ec8439be027c56b94bb30358eafd31248ec2cd39
takabayap <takabayap@gmail.com>
2024-04-28 01:27:56 +0900

    update: add theme switcher to navbar

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -19,9 +19,16 @@ export default function RootLayout({
     <html suppressHydrationWarning data-theme="cupcake">
       <body className={inter.className}>
         <ThemeProvider>
-          <div className="navbar bg-base-100 sticky top-0 z-30 bg-primary-content">
+          <div className="navbar bg-primary sticky top-0 z-30">
             <div className="flex-1">
-              <a className="btn btn-ghost text-xl btn-primary">ReRecipe</a>
+              <a className="btn text-xl btn-primary">ReRecipe</a>
+            </div>
+            <div className="flex-none">
+              <label className="flex cursor-pointer gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
+                <input type="checkbox" value="bumblebee" className="toggle theme-controller" />
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+              </label>
             </div>
             <div className="flex-none">
               <button className="btn btn-square btn-ghost">

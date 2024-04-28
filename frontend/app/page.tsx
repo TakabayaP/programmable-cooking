8f368b34b0381601237fdcd536fcf761b9b5388e
takabayap <takabayap@gmail.com>
2024-04-28 14:57:22 +0900

    update: substract padding

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   };
   return (
     <main className="flex min-h-screen justify-center">
-      <div className="flex flex-col items-center justify-between p-24 w-full lg:w-[1024px]">
+      <div className="flex flex-col items-center justify-between p-12 w-full lg:w-[1024px]">
         <h1 className="text-4xl text-left bg-primary w-full my-4">料理のレシピを動的に</h1>
         <h2 className="text-2xl text-left text-secondary w-full my-2">レシピ一覧</h2>
         <ul className="grid grid-cols-1 md:grid-cols-2 gap-8">

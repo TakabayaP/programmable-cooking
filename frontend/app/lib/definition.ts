a771b41ce033b39187e6108bb7a31e21107f6c79
takabayap <takabayap@gmail.com>
2024-04-28 10:33:40 +0900

    add: definitions

---
+++ frontend/app/lib/definition.ts
@@ -0,0 +1,13 @@
+export type Recipe = {
+    id: number;
+    title: string;
+    description: string;
+    image: Image;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export type Image = {
+    src: string;
+    alt: string;
+}

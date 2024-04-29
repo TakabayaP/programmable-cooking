909e8f6445db47ba9115eea20e95b0632de567e2
takabayap <takabayap@gmail.com>
2024-04-29 15:33:10 +0900

    update: enable SSG

--- frontend/app/recipies/[id]/page.tsx
+++ frontend/app/recipies/[id]/page.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { RecipeView } from "@/app/ui/recipe-view";
 
+export function generateStaticParams() {
+    return [{id: "0"}]
+}
+
 export default function Recipe({ params }: { params: { id: string } }) {
     return (
         <div>

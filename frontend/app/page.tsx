191b43f2b53c80db258a2826aa53b70ce2ab649e
takabayap <takabayap@gmail.com>
2024-04-29 14:47:35 +0900

    clean

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
-import { RecipeCard } from '@/app/ui/recipe-card';
-import { Recipe } from '@/app/lib/definition';
+import { Recipe } from "@/app/lib/definition";
+import { RecipeCard } from "@/app/ui/recipe-card";
 
 export default function Home() {
   const recipe: Recipe = {
@@ -12,25 +12,23 @@ export default function Home() {
     updatedAt: '更新日時'
   };
   return (
-    <main className="flex min-h-screen justify-center">
-      <div className="flex flex-col items-center justify-between p-12 w-full lg:w-[1024px]">
-        <h1 className="text-4xl text-left bg-primary w-full my-4">料理のレシピを動的に</h1>
-        <h2 className="text-2xl text-left text-secondary w-full my-2">レシピ一覧</h2>
-        <ul className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <li>
-            <RecipeCard recipe={recipe} />
-          </li>
-          <li>
-            <RecipeCard recipe={recipe} />
-          </li>
-          <li>
-            <RecipeCard recipe={{...recipe,title:"焼豚チャーハン", description: "特殊なチャーハンのレシピです。具材には特別なスパイスを使用し、独特の風味を出しています。炒める際には高温で素早く調理し、ご飯がパラパラになるように仕上げます。"}} />
-          </li>
-          <li>
-            <RecipeCard recipe={recipe} />
-          </li>
-        </ul>
-      </div>
-    </main>
+    <div>
+      <h1 className="text-4xl text-left bg-primary w-full my-4">料理のレシピを動的に</h1>
+      <h2 className="text-2xl text-left text-secondary w-full my-2">レシピ一覧</h2>
+      <ul className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <li>
+          <RecipeCard recipe={recipe} />
+        </li>
+        <li>
+          <RecipeCard recipe={recipe} />
+        </li>
+        <li>
+          <RecipeCard recipe={{ ...recipe, title: "焼豚チャーハン", description: "特殊なチャーハンのレシピです。具材には特別なスパイスを使用し、独特の風味を出しています。炒める際には高温で素早く調理し、ご飯がパラパラになるように仕上げます。" }} />
+        </li>
+        <li>
+          <RecipeCard recipe={recipe} />
+        </li>
+      </ul>
+    </div>
   );
 }
--- frontend/app/recipies/[id]/page.tsx
+++ frontend/app/recipies/[id]/page.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React from "react";
+import { RecipeView } from "@/app/ui/recipe-view";
 
 export default function Recipe({ params }: { params: { id: string } }) {
     return (
         <div>
+            <RecipeView />
             This is the recipe page for id: {params.id}
         </div>
     );
